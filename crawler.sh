558771a75c9ed3ae3e7ed417f28a80ba460d3246
HWLin <hwlin1414@gmail.com>
2016-06-14 22:36:54 +0800

    mod crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -1,36 +1,73 @@
 #!/bin/sh
-/usr/local/bin/python2 main.py \
+#/usr/bin/env python main.py \
+#    -u http://www.cs.ccu.edu.tw/ \
+#    -d '[^/]*.cs.ccu.edu.tw' \
+#    -t 10 \
+#    -T 3 \
+#    -l 128 \
+#    -F drupal \
+#    -f iso \
+#    -f flv \
+#    -f avi \
+#    -f exe \
+#    -f wav \
+#    -f pdf \
+#    #-f mp3 \
+#    #-f 7z \
+#    #-f rar \
+#    #-f tgz \
+#    #-f tar.gz \
+#    #-f zip \
+#    #-f docx \
+#    #-f doc \
+#    #-f xls \
+#    #-f xlsx \
+#    #-f pptx \
+#    #-f ppt \
+#    #-f pdf \
+#    #-f JPG \
+#    #-f jpg \
+#    #-f png \
+#    #-f css \
+#    #-f js \
+#    #-f gif \
+#    #-f ico \
+#    #-f bmp \
+#    #-w res \
+
+/usr/bin/env python main.py \
     -u http://www.cs.ccu.edu.tw/ \
     -d '[^/]*.cs.ccu.edu.tw' \
-    -t 10 \
-    -T 2 \
-    -l 128 \
     -F drupal \
+    -f pdf \
     -f iso \
     -f flv \
     -f avi \
     -f exe \
     -f wav \
+    -f mp3 \
+    -f 7z \
+    -f rar \
+    -f tgz \
+    -f tar.gz \
+    -f zip \
+    -f docx \
+    -f doc \
+    -f xls \
+    -f xlsx \
+    -f pptx \
+    -f ppt \
     -f pdf \
-    #-f mp3 \
-    #-f 7z \
-    #-f rar \
-    #-f tgz \
-    #-f tar.gz \
-    #-f zip \
-    #-f docx \
-    #-f doc \
-    #-f xls \
-    #-f xlsx \
-    #-f pptx \
-    #-f ppt \
-    #-f pdf \
-    #-f JPG \
-    #-f jpg \
-    #-f png \
-    #-f css \
-    #-f js \
-    #-f gif \
-    #-f ico \
-    #-f bmp \
-    #-w res \
+    -f JPG \
+    -f jpg \
+    -f png \
+    -f css \
+    -f js \
+    -f gif \
+    -f ico \
+    -f bmp \
+    -w res \
+
+/usr/bin/env python main.py \
+    -u http://www.csie.ntu.edu.tw/ \
+    -d '[^/]*.csie.ntu.edu.tw' \
